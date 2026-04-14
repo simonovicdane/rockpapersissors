e3fc8fa2055806d30d5e276c9897f58d740681f7
simonovicdane <simonovicdane@gmail.com>
2026-04-14 08:17:19 -0500

    Added some basic messeges, a main loop, and an exit flag so that when the player wants to quit they can do so without having to force quit

--- rockpapersissors.py
+++ rockpapersissors.py
@@ -1,2 +1,28 @@
 # A basic rock paper sissors game that can be ran from the terminal
-# I am using this to learn git so please bare with me 
+# I am using this to learn git so please bare with me
+
+exit_flag = False
+line_count = 100
+line_string = "-"
+
+def mainGameLoop():
+    print("hello")
+    print("fart")
+    global exit_flag
+    exit_flag = True
+
+
+print(f"""{line_string*line_count}
+      
+    Welcome to my rock paper sissors game, a simple verision I am using to learn git
+
+{line_string*line_count}""")
+
+while not exit_flag:
+    mainGameLoop()
+
+print(f"""{line_string*line_count}
+      
+    Thanks for playing!
+      
+{line_string*line_count} """)
